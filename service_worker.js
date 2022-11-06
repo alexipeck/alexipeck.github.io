e52425464815ec942ce8dd443019b0267d911965
Alexi Peck <alexinicolaspeck@gmail.com>
2022-11-07 00:49:22 +1100

    .

--- service_worker.js
+++ service_worker.js
@@ -2,8 +2,8 @@ var cacheName = 'yew-pwa';
 var filesToCache = [
   './',
   './index.html',
-  './index-f8c4dec785e3d1ea.js',
-  './index-f8c4dec785e3d1ea_bg.wasm',
+  './index-45372f53c3c38b6a.js',
+  './index-45372f53c3c38b6a_bg.wasm',
 ];
 
 /* Start the service worker and cache all of the app's content */
