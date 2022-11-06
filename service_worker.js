dc0f4f5e8dc2afeefc2cd9b2afe7378d1bcbd3bc
Alexi Peck <alexinicolaspeck@gmail.com>
2022-11-06 13:48:30 +1100

    .

--- service_worker.js
+++ service_worker.js
@@ -2,8 +2,8 @@ var cacheName = 'yew-pwa';
 var filesToCache = [
   './',
   './index.html',
-  './index-34a68bf856e2b59.js',
-  './index-34a68bf856e2b59_bg.wasm',
+  './index-6a1b6d15c05c55ba.js',
+  './index-6a1b6d15c05c55ba_bg.wasm',
 ];
 
 
